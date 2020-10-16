5445df699745ca297ef361d934ed094c6dc514b0
Guilherme Longhini <guilherme.longhini@gmail.com>
2020-10-16 03:06:13 -0300

    Manutenção

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('openMenu', function(e){
+    //It does something when the menu is opened
+    //You can access name of menu in: e.detail.menu
+  })
+  
+  document.addEventListener('closeMenu', function(e){
+    //It does something when the menu is closed.
+    //You can access name of menu in: e.detail.menu
+  })
